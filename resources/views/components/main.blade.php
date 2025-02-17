7f84ae79639b81409c34951c3fcca4fb7c6a4543
GaspareDantone <dantogas@gmail.com>
2025-02-17 16:50:08 +0100

    start

---
+++ resources/views/components/main.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+  <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.23/dist/full.min.css" rel="stylesheet" type="text/css" />
+<script src="https://cdn.tailwindcss.com"></script>
+  <link href="/src/styles.css" rel="stylesheet">
+</head>
+<body class="bg-sky-50">
+     <x-navbar />
+     {{ $slot}}
+     <x-footer />
+</body>
+</html>
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,18 @@
+
+<x-main>
+<div class="hero  min-h-screen">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+      class="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h2 class="text-5xl font-bold">Benvenuto in Project 0</h2>
+      <h1 class="text-5xl font-bold">Sono Gaspare D'Antone</h1>
+      <p class="py-6">
+       Full-Stack Web Developer
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+
+</x-main>
