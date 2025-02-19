979a24d1f031078c40a86ee579ed328b786cb258
GaspareDantone <dantogas@gmail.com>
2025-02-19 16:23:05 +0100

    sezione tec

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,6 +1,6 @@
 
 <x-main>
-<div class="hero  min-h-screen -mt-15 ">
+  <div class="hero  min-h-screen -mt-15 ">
   <div class="hero-content flex-col lg:flex-row-reverse ">
     <img
       src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
@@ -13,6 +13,141 @@
       <button class="btn btn-primary">Get Started</button>
     </div>
   </div>
-</div>
+  </div>
+<!-- sezione card e tecnologie -->
+  <div class=" grid grid-cols-3 ">
+    <!-- sezione1 -->
+    <div class="bg-sky-50 text-center text-3xl">
+      <p class="">
+        Queste sono le tecnologie che al momento sto utilizzando per creare i miei progetti,col tempo implementero questa lista arricchendo il mio bagaglio di conoscenze per creare progetti sempre nuovi e diversi
+      </p>
+
+    </div>
+    <!-- fine sezione 1 -->
+ 
+   <!-- inizio sezione 2 -->
+    <div class="col-span-2 bg-sky-50">
+      <div class="grid grid-cols-6 gap-4 p-4">
+        
+        <div class="card card-compact bg-base-100 w-40  m-2 col-span-1">
+            <figure>
+            <img src="/storage/media/git.png" alt="">
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Git</h2>
+              
+            </div>  
+        </div>
+        <div class="card card-compact bg-base-100  w-40 m-2 col-span-1">
+            <figure>
+            <img src="/storage/media/css3.png" alt="">
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Css</h2>
+              
+            </div>  
+        </div>
+        <div class="card card-compact bg-base-100 w-40  m-2 col-span-1">
+            <figure>
+            <img src="/storage/media/js.png" alt="">
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Javascript</h2>
+              
+            </div>  
+        </div>
+        <div class="card card-compact bg-base-100 w-40  m-2 col-span-1">
+            <figure>
+            <img src="/storage/media/bootstrap.png" alt="">
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Bootstrap</h2>
+              
+            </div>  
+        </div>
+        <div class="card card-compact bg-base-100 w-40  m-2 col-span-1">
+            <figure>
+            <img src="/storage/media/git.png" alt="">
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Shoes!</h2>
+              
+            </div>  
+        </div>
+        <div class="card card-compact bg-base-100 w-40  m-2 col-span-1">
+            <figure>
+            <img src="/storage/media/git.png" alt="">
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Git</h2>
+              
+            </div>  
+        </div>
+        <div class="card card-compact bg-base-100 w-40  m-2 col-span-1">
+            <figure>
+            <img src="/storage/media/css3.png" alt="">
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Css</h2>
+              
+            </div>  
+        </div>
+        <div class="card card-compact bg-base-100  w-40 m-2 col-span-1">
+            <figure>
+            <img src="/storage/media/js.png" alt="">
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Javascript</h2>
+              
+            </div>  
+        </div>
+        <div class="card card-compact bg-base-100  w-40 m-2 col-span-1">
+            <figure>
+            <img src="/storage/media/bootstrap.png" alt="">
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Bootstrap</h2>
+              
+            </div>  
+        </div>
+        <div class="card card-compact bg-base-100  w-40 m-2 col-span-1">
+            <figure>
+            <img src="/storage/media/git.png" alt="">
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Shoes!</h2>
+              
+            </div>  
+        </div>
+        <div class="card card-compact bg-base-100  w-40 m-2 col-span-1">
+            <figure>
+            <img src="/storage/media/git.png" alt="">
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Shoes!</h2>
+              
+            </div>  
+        </div>
+        
+      </div>
+          
+
+        
+          
+
+        
+        
+        
+    </div>
+  </div>
+       
+  
+    
+
+
+
+  
+
+ 
 
 </x-main>
