199a4c5e25e2c940573ce95cdc2b9d3b9613fc09
GaspareDantone <dantogas@gmail.com>
2025-07-11 15:55:26 +0200

    project

--- resources/views/aboutme.blade.php
+++ resources/views/aboutme.blade.php
@@ -7,10 +7,15 @@
     />
     <div>
       <h1 class="text-5xl font-bold text-center">Mi presento</h1>
-      <p class="py-6 text-center ">
+      <p class="py-6 text-center text-3xl">
         Ciao piacere di conoscerti sono  Gaspare D'Antone. Da sempre ho avuto una forte passione per tutto quello che riguarda le nuove tecnlogie Hardware e Software è questo mi ha portato durante le mie esperienze lavorative ad occuparmi anche dell'assemblaggio Pc,ma recentemente ho investito il mio tempo e le mia passione in un corso per acquisire le competenze necessarie per diventare un Junior Full Stack Web Developer 
       </p>
-      <button class="btn btn-primary">Torna indietro</button>
+      <h3 class="text-4xl font-bold text-center">Studi</h3>
+      <p class="py-6 text-center text-3xl">
+        In Aulab nel percorso di studi ho appreso diverse tecnologie utilizzate nell'ambiente Web tra cui HTML, CSS, JAVASCRIPT, BOOTSTRAP.PHP e il framework LARAVEL,per il versionamento del codice utilizzo GIT con GITUB, al momento sto studiando TAILWIND con il quale è realizzato questo progetto
+      </p>
+        
+      <a href="{{ route('homepage')}}" class="btn btn-outline btn-error">ComeBack</a>
     </div>
   </div>
 </div>
--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -1,3 +1,71 @@
 <x-main>
     
+    <div class=" bg-red-100   min-h-screen  ">
+        <div>I proggetti al momento sviluppati durante il mio corso di studi e lavori previta</div>
+        <div class="  flex justify-evenly">
+            <div class=" grid lg:grid-cols-3 grid-cols-1">
+ <!-- firstcard -->
+                <div class="card bg-base-100 w-96 shadow-sm mt-20 mx-5">
+                 <figure>
+                    <img
+                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    alt="Shoes" />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Card Title</h2>
+                        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+<!-- SECONDA CARD -->
+                <div class="card bg-base-100 w-96 shadow-sm mt-20 mx-5">
+                 <figure>
+                    <img
+                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    alt="Shoes" />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Card Title</h2>
+                        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                <!-- terza -->
+                 <div class="card bg-base-100 w-96 shadow-sm mt-20 mx-5">
+                 <figure>
+                    <img
+                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    alt="Shoes" />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Card Title</h2>
+                        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            
+        
+        
+        
+        
+        
+        </div>
+       
+        
+            
+        
+            
+      
+        
+        
+    
+    </div>
 </x-main>
