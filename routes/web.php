2b72baa0127d1a4b52a9491a88809dd8dc2e39c9
GaspareDantone <dantogas@gmail.com>
2025-08-17 13:22:21 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,22 @@
 <?php
 
-use App\Http\Controllers\Pagecontroller;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/',[Pagecontroller::class, 'homepage'] )->name('homepage');
+Route::get('/',[PageController::class, 'homepage'] )->name('homepage');
 
-Route::get('/project',[Pagecontroller::class, 'project'] )->name('project');
+Route::get('/project',[PageController::class, 'project'] )->name('project');
 
-Route::get('/aboutme',[Pagecontroller::class, 'aboutme'] )->name('aboutme');
+Route::get('/aboutme',[PageController::class, 'aboutme'] )->name('aboutme');
 
 
-Route::get('/contact',[Pagecontroller::class, 'contact'] )->name('contact');
-Route::post('/send',[Pagecontroller::class, 'send'] )->name('send');
+Route::get('/contact',[PageController::class, 'contact'] )->name('contact');
+Route::post('/send',[PageController::class, 'send'] )->name('send');
 
-Route::get('/thankyou',[Pagecontroller::class, 'thankyou'] )->name('emails.thankyou');
+Route::get('/thankyou',[PageController::class, 'thankyou'] )->name('emails.thankyou');
 
-Route::get('/downloadcv',[Pagecontroller::class, 'downloadcv'] )->name('downloadcv');
+Route::get('/downloadcv',[PageController::class, 'downloadcv'] )->name('downloadcv');
 
 
 
